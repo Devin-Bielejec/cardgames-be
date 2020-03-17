77ab9893c3a84d2c6de2f10257da7e653fb3ef23
Devin Bielejec <devin.bielejec@gmail.com>
2020-03-17 10:31:55 -0400

    basic chat complete
    Co-authored-by: Seth Lachman <lachmanseth17@gmail.com>

--- index.js
+++ index.js
@@ -1,6 +1,27 @@
-const server = require('./server');
-const dotenv = require('dotenv').config();
+const server = require("./server");
+const dotenv = require("dotenv").config();
+
+const http = require("http").createServer(server);
+const io = require("socket.io")(http);
 
 const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => console.log(`WE ARE LISTENING ON PORT: ${PORT}!`))
+io.on("connection", socket => {
+  socket.on("add user", name => {
+    socket.broadcast.emit("user joined", { name: name });
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+
+  socket.on("chat message", msg => {
+    io.emit("chat message", msg);
+  });
+});
+
+http.listen(PORT, () => {
+  console.log(`listening on *:${PORT}~`);
+});
+
+// server.listen(PORT, () => console.log(`WE ARE LISTENING ON PORT: ${PORT}!`))
